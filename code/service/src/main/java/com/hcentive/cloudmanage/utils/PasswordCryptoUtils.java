1763c5bef829edc286a3e0fa7ea4772568a5a659
vaibhav.gupta <vaibhav.gupta@hcentive.com>
2016-01-21 16:05:22 +0530

    Fixing PasswordCryptoUtils

--- code/service/src/main/java/com/hcentive/cloudmanage/utils/PasswordCryptoUtils.java
+++ code/service/src/main/java/com/hcentive/cloudmanage/utils/PasswordCryptoUtils.java
@@ -21,7 +21,7 @@ public class PasswordCryptoUtils {
 	}
 	
 	public static void main(String[] args){
-			String password = "Hcentive@12345678";
+			String password = "Qwerty123";
 			System.out.println(encryptPassword(password));
 			
 		}
