bbfd73860d6d5b94001f33437c6ec60366a8f584
Maninder Singh <maninder.singh@hcentive.com>
2016-12-14 14:03:57 +0530

    CLOUD-25 : unit test configuration for ui

--- code/ui/test/spec/controllers/audit.js
+++ code/ui/test/spec/controllers/audit.js
@@ -5,19 +5,34 @@ describe('Controller: AuditCtrl', function () {
   // load the controller's module
   beforeEach(module('cloudmanageApp'));
 
-  var AuditCtrl,
-    scope;
+  var AuditCtrl,scope,AuditServiceMock,
+    auditData = [{"eventType":"created","args":"audit:args","userName":"dummy_user","eventTime":Date()},
+      {"eventType":"updated","args":"audit:args","userName":"dummy_user","eventTime":Date()},
+      {"eventType":"deleted","args":"audit:args","userName":"dummy_user","eventTime":Date()},
+      {"eventType":"updated","args":"audit:args","userName":"dummy_user","eventTime":Date()},
+      {"eventType":"created","args":"audit:args","userName":"dummy_user","eventTime":Date()}];
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  // Initialize the controller and it's dependencies
+  beforeEach(inject(function ($controller, $rootScope,_auditService_) {
     scope = $rootScope.$new();
+    AuditServiceMock = _auditService_;
+    spyOn(AuditServiceMock, 'getList').and.returnValue({then:function(callback){
+      return callback({data:auditData});
+    }});
     AuditCtrl = $controller('AuditCtrl', {
-      $scope: scope
-      // place here mocked dependencies
+      $scope: scope,
+      auditService : AuditServiceMock
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(AuditCtrl.awesomeThings.length).toBe(3);
+  it("AuditCtrl : should be defined", function () {
+    expect(AuditCtrl).toBeDefined();
+  });
+
+  it("AuditCtrl : loadMore method",function(){
+      AuditCtrl.list = [];
+      expect(AuditCtrl.list.length).toBe(0);
+      AuditCtrl.loadMore();
+      expect(AuditCtrl.list.length).toBe(5);
   });
 });
